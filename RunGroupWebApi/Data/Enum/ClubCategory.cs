037c2609e293a0cc9e62aa4a8ef2567e69cf6f81
Ihor <flisfish@ukr.net>
2023-11-05 14:11:43 +0100

    Initial commit

---
+++ RunGroupWebApi/Data/Enum/ClubCategory.cs
@@ -0,0 +1,10 @@
+namespace RunGroupWebApi.Data.Enum;
+
+public enum ClubCategory
+{
+    RoadRunner,
+    Womens,
+    City,
+    Trail,
+    Endurance
+}
---
+++ RunGroupWebApi/Data/Enum/RaceCategory.cs
@@ -0,0 +1,10 @@
+namespace RunGroupWebApi.Data.Enum;
+
+public enum RaceCategory
+{
+    Marathon,
+    Ultra,
+    FiveK,
+    TenK,
+    HalfMarathon
+}
